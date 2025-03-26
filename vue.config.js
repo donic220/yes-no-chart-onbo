4f2c93e2f43657b8ef9a131f40944ae0422968cc
Narushima Daiki <donic220@gmail.com>
2025-03-26 22:22:47 +0900

    デプロイ用の設定追加

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // GitHub Pages用の設定
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/yesno-chart/'
+    : '/',
   chainWebpack: (config) => {
     config.plugin('define').tap((definitions) => {
       Object.assign(definitions[0], {
